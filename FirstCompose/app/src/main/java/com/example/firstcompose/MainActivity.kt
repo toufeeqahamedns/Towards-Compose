af3c02276c9d2a110a856116dedf7d63a1c285d0
toufeeqahamedns <toufeeq.nas@gmail.com>
2021-03-09 22:42:20 +0530

    State management

--- FirstCompose/app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ FirstCompose/app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,17 +39,32 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent() {
+fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+    val counterState = remember { mutableStateOf(0) }
+
     Column {
-        Greeting(name = "Android")
-        Divider(color = Color.Black)
-        Greeting(name = "there")
+        for (name in names) {
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount -> counterState.value = newCount }
+        )
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text("I've been clicked $count times")
+    }
 }
 
 @Preview(showBackground = true)
