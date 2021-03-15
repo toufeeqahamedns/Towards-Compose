e11a81f7a6dd13788898f2039e29a7f8930da766
toufeeqahamedns <toufeeq.nas@gmail.com>
2021-03-15 12:33:51 +0530

    Lazy List with image loading

--- ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
+++ ComposeLayouts/app/src/main/java/com/example/composelayouts/MainActivity.kt
@@ -6,13 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,6 +25,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composelayouts.ui.theme.ComposeLayoutsTheme
+import dev.chrisbanes.accompanist.coil.CoilImage
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +39,13 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun PhotographerCard() {
+
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
         // topBar is slot in Scaffold
         topBar = {
@@ -50,12 +64,37 @@ fun PhotographerCard() {
             )
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding))
+        Column {
+            Row {
+                Button(onClick = {
+                    coroutineScope.launch {
+                        // 0 is the first item index
+                        scrollState.animateScrollToItem(0)
+                    }
+                }) {
+                    Text("Scroll to the top")
+                }
+
+                Button(onClick = {
+                    coroutineScope.launch {
+                        // listSize - 1 is the last index of the list
+                        scrollState.animateScrollToItem(listSize - 1)
+                    }
+                }) {
+                    Text("Scroll to the end")
+                }
+            }
+            LazyColumn(state = scrollState) {
+                items(100) {
+                    BodyContent(Modifier.padding(innerPadding), it)
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun BodyContent(modifier: Modifier = Modifier) {
+fun BodyContent(modifier: Modifier = Modifier, index: Int) {
     Row(
         modifier = modifier
             // The order in which modifiers appended will make the UI behave differently
@@ -72,17 +111,18 @@ fun BodyContent(modifier: Modifier = Modifier) {
             color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
         ) {
             // Image goes here
+            CoilImage(
+                data = "https://developer.android.com/images/brand/Android_Robot.png",
+                contentDescription = "Android Logo",
+                modifier = Modifier.size(50.dp)
+            )
         }
         Column(
             modifier = Modifier
-
                 .align(Alignment.CenterVertically)
                 .padding(start = 8.dp)
         ) {
-            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text("3 minutes ago", style = MaterialTheme.typography.body2)
-            }
+            Text("Item #$index", fontWeight = FontWeight.Bold)
         }
     }
 }
