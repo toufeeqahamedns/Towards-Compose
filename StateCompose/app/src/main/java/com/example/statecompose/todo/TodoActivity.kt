0d64b4a9bc9c5ad37453351e411efc1e88547575
toufeeqahamedns <toufeeq.nas@gmail.com>
2021-05-23 20:30:58 +0530

    Managing and hoisting state

--- StateCompose/app/src/main/java/com/example/statecompose/todo/TodoActivity.kt
+++ StateCompose/app/src/main/java/com/example/statecompose/todo/TodoActivity.kt
@@ -21,6 +21,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import com.example.statecompose.ui.theme.StateComposeTheme
 
 class TodoActivity : AppCompatActivity() {
@@ -32,9 +35,20 @@ class TodoActivity : AppCompatActivity() {
         setContent {
             StateComposeTheme {
                 Surface {
-                    // TODO: build the screen in compose
+                    TodoActivityScreen(todoViewModel)
                 }
             }
         }
     }
+}
+
+@Composable
+fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+    val items: List<TodoItem> by todoViewModel.todoItems.observeAsState(listOf())
+
+    TodoScreen(
+        items = items,
+        onAddItem = todoViewModel::addItem,
+        onRemoveItem = todoViewModel::removeItem
+    )
 }
