f5a0c25adc5d5015645f9bae57270b59dff8143e
toufeeqahamedns <toufeeq.nas@gmail.com>
2022-11-24 18:08:23 +0530

    basic state changes

--- 03 - StateCompose/app/src/main/java/com/example/statecompose/MainActivity.kt
+++ 03 - StateCompose/app/src/main/java/com/example/statecompose/MainActivity.kt
@@ -18,25 +18,53 @@ package com.example.statecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.example.statecompose.ui.theme.BasicStateCodelabTheme
+import androidx.compose.ui.unit.dp
+import com.example.statecompose.ui.theme.StateComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicStateCodelabTheme {
+            StateComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    WellnessScreen()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text(text = "You've had $count glasses", modifier = modifier.padding(16.dp))
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
         }
     }
 }
--- 03 - StateCompose/app/src/main/java/com/example/statecompose/ui/theme/Theme.kt
+++ 03 - StateCompose/app/src/main/java/com/example/statecompose/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun BasicStateCodelabTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun StateComposeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
